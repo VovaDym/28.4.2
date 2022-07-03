906ea113a1bb459ae21628d773ae0bdf1f0a4298
lebed <lebed@DESKTOP-BKIICKL>
2022-07-03 11:30:24 +0300

    Add project files.

---
+++ include/train.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+
+class Train
+{
+public:
+    Train(std::string name, int time) : name(name), timeOnWay(time) {}
+    
+    std::string getName()
+    {
+        return name;
+    }
+
+    int getTime()
+    {
+        return timeOnWay;
+    }
+private:
+
+    std::string name = "unknown";
+    int timeOnWay = 0;
+};
---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include "train.h"
+
+std::mutex busyRailwayTrack;
+
+void goTrain(Train* train)
+{
+    std::this_thread::sleep_for(std::chrono::seconds(train->getTime()));
+    if (busyRailwayTrack.try_lock())
+    {
+        busyRailwayTrack.unlock();
+    }
+    else
+    {
+        std::cout << "    Train " << train->getName() << " is waiting for the release of the track" << std::endl;
+    }
+        
+    busyRailwayTrack.lock();
+    std::cout << " Train " << train->getName() << " has arrived at the railway station" << std::endl;
+    std::cout << "Enter \depart\ in order for train " << train->getName() << " to depart" << std::endl;
+    std::string depart = "";
+    while (depart != "depart")
+    {
+        std::cin >> depart;
+    }
+        
+    std::cout << "The way is clear!Train " << train->getName() << " has left" << std::endl;
+    busyRailwayTrack.unlock();
+}
+
+int main()
+{
+    constexpr int numberTrain = 3;
+    std::vector<Train*> trains;
+    for (int i = 0; i < numberTrain; ++i)
+    {
+        std::cout << "Train " << i + 1 << std::endl;
+        std::cout << "Enter name train A,B or C: ";
+        std::string name;
+        std::cin >> name;
+        std::cout << "Enter time on way (sec): ";
+        int time;
+        std::cin >> time;
+        trains.push_back(new Train(name, time));
+    }
+
+    system("cls");
+
+    std::vector <std::thread> trainThread;
+    for (int i = 0; i < trains.size(); ++i)
+    {
+        trainThread.push_back(std::thread(goTrain, trains[i]));
+    }
+        
+    for (auto& tr : trainThread)
+    {
+        tr.join();
+    }    
+}
